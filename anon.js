8b4ad95be20a67a68f623a4e7e53cb522083b846
Siddhi Sanjay Bodake <siddhii@Siddhis-MacBook-Air.local>
2026-04-25 09:54:19 +0530

    eventloops

---
+++ anon.js
@@ -0,0 +1,61 @@
+//function statemnet
+function a() {
+    console.log("a");
+
+}
+a();
+
+//function expression
+
+var b = function () {
+    console.log("b");
+
+}
+b();
+
+//function declaration
+
+function a() {
+    console.log("a");
+
+}
+a();
+
+//anonymous function
+
+function () {
+    console.log("c");
+
+}
+
+//named function expression
+var b = function c() {
+    console.log("c");
+
+}
+
+//differnce between parameters and argumnets
+
+function a(param1, param2) {
+    console.log(param1, param2);
+
+}
+a(1, 2);
+
+//first class functions
+
+var b = function (param1) {
+    console.log(param1);
+
+}
+function xyz() {
+
+}
+b(xyz);
+//ability to use function use as values is called first class fucntions
+//first class citizens
+
+
+//arrow functions
+
+
