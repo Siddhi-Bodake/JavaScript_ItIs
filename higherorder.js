618afb5a0d5e93c5873000e76f3dd6233c15798c
Siddhi Sanjay Bodake <siddhii@Siddhis-MacBook-Air.local>
2026-04-26 13:01:34 +0530

    highorderfunctions

--- higherorder.js
+++ higherorder.js
@@ -11,6 +11,30 @@ y(x);
 
 
 const radius = [3,1,2,4];
+const area = function (radius){
+    return Math.PI *radius*radius;
+};
+
+const circumference = function (radius){
+    return 2 * Math.PI * radius;
+};
+
+const diameter = function (radius){
+    return 2 * radius;
+};
+
+const calculate = function (radius,logic){
+    const output = [];
+    for(let i =0 ; i<radius.length; i++){
+        output.push(logic(radius[i]))
+    }
+    return output;
+};
+
+console.log(calculate(radius, area));
+console.log(calculate(radius, circumference));
+console.log(calculate(radius, diameter));
+
 
 const calculateArea = function (radius){
     const output = [];
@@ -21,3 +45,23 @@ const calculateArea = function (radius){
 }
 
 console.log(calculateArea(radius));
+
+const  calculateCircumference = function (radius){
+    const output = [];
+    for(let i =0 ; i<radius.length; i++){
+        output.push(2 * Math.PI * radius[i])
+    }
+    return output;
+}
+console.log(calculateCircumference(radius));
+
+
+//DRY principle
+
+//map 
+
+console.log(radius.map(area));
+
+
+//what is Array.prototype.calculate  and tell me what is this.length and all ?? 
+ 
